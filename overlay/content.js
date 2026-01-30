ddc4d55e9f568e2599804bc4484746f4a16f2add
cait <155276726+caitroach@users.noreply.github.com>
2026-01-30 13:26:37 -0500

    added reddit lmao

---
+++ overlay/content.js
@@ -0,0 +1,32 @@
+(() => {
+  if (document.getElementById("vo-overlay")) return;
+
+ //this isn't a groundbreaking discovery but u can easily add blocked websites in the json 
+  const overlay = document.createElement("div");
+  overlay.id = "vo-overlay";
+
+  const box = document.createElement("div");
+  box.className = "box";
+
+  const video = document.createElement("video");
+  video.controls = false;
+  video.autoplay = true;       
+  video.muted = true;   // cant force unmute on chrome, but u can make it so when the user clicks it starts screaming        
+  video.loop = true;
+  video.playsInline = true;
+
+  video.src = chrome.runtime.getURL("skeleton.mp4");
+
+  box.appendChild(video);
+  overlay.appendChild(box);
+  document.documentElement.appendChild(overlay);
+
+  overlay.addEventListener("click", () => {
+  if (video.muted) {
+    video.muted = false;
+    video.volume = 1.0;
+  }
+});
+
+  document.addEventListener("keydown", onKeyDown, true);
+})();
